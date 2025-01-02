0513f18ab44c5cef96fa5d949f010601189b2060
Siyabonga Mbuyisa-20491621 <160500265+Siyabonga-Mbuyisa-u20491621@users.noreply.github.com>
2025-01-02 22:55:22 +0200

    PWA activated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+// export default nextConfig;
+
+
+
+module.exports = withPWA(nextConfig);
