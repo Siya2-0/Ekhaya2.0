bcf4d0eaca5f7f171f0f70c2903e39668df6501d
Segadimane-Makau <161027953+Segadimane-Makau@users.noreply.github.com>
2025-01-11 17:44:23 +0200

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -5,7 +5,7 @@ import { createClient } from "@/utils/supabase/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-const origin = process.env.NEXT_PUBLIC_API_BASE_URL
+const defaultUrl = process.env.NEXT_PUBLIC_API_BASE_URL
   ? `https://${process.env.NEXT_PUBLIC_API_BASE_URL}`
   : "http://localhost:3000";
 
@@ -69,7 +69,7 @@ export const signInAction = async (formData: FormData) => {
 export const forgotPasswordAction = async (formData: FormData) => {
   const email = formData.get("email")?.toString();
   const supabase = await createClient();
-  // const origin = (await headers()).get("origin");
+  const origin = (await headers()).get("origin");
   const callbackUrl = formData.get("callbackUrl")?.toString();
 
   if (!email) {
